a86d0bb870d3c1df3b6dfc593d85d3b7b24e525d
xraybrain <xraybrain@gmail.com>
2021-06-13 19:42:15 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+const { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -32,6 +33,24 @@ app.get('/todos', async (req, res) => {
     console.log(error);
     res.status(400).send(error);
   }
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(
+    (todo) => {
+      if (!todo) return res.status(404).send();
+
+      res.send({ todo });
+    },
+    (e) => {
+      res.status(400).send();
+    }
+  );
 });
 
 app.listen(3000, () => {
