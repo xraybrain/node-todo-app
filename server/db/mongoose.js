d555afe1c158cf9a25b287e3269a0bed1fa32d5c
xraybrain <xraybrain@gmail.com>
2021-06-13 21:58:19 +0100

    Add error handler for DB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',
-  {
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-);
+  })
+  .then(() => console.log('connected to mongodb'))
+  .catch((e) => console.log(e));
 
 module.exports = { mongoose };
