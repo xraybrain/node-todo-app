5158fc3b9cd513e55febcb52c3dafd4b145aace9
xraybrain <xraybrain@gmail.com>
2021-06-13 08:00:01 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log(`Started on port 3000`);
+});
