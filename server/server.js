44d2e418c4273c32fc158ac66e88d0d2a80ab8fe
xraybrain <xraybrain@gmail.com>
2021-06-15 21:30:27 +0100

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -54,6 +54,24 @@ app.get('/todos/:id', (req, res) => {
   );
 });
 
+app.delete('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(404).send();
+
+  Todo.findByIdAndDelete(id).then(
+    (todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send(todo);
+    },
+    (err) => {
+      res.status(400).send();
+    }
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
