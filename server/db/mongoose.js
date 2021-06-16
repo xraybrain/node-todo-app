227f5a9656c1ac2e0902af23f246e4e39f308c4e
xraybrain <xraybrain@gmail.com>
2021-06-16 02:58:22 +0100

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 
@@ -7,7 +8,7 @@ const { User } = require('./models/user');
 const { ObjectID } = require('mongodb');
 
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
